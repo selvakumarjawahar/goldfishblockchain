c2ad52c60f558e0de2bcbd238cddd079b02bdd10
sjawahar <selvakumarj@diipl.com>
2018-11-04 02:10:18 +0530

    CrytoCPP is successfully ported on to Cmake, tested on VS2017, need to test on other platforms and compilers. The tests for cryptocpp are compiling but need to add resources for it to run

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(char* argv[], int argc)
+{
+	std::cout << "Entering into Exiting world of GoldFish BlockChain !!!!!!!!!!" << '\n';
+    return 0;
+}
